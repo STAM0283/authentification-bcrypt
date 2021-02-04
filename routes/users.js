8416f94bc92d4821f8883ecdd02b4ac29c723002
stambouli <aminestambouli90@yahoo.fr>
2021-02-04 22:10:16 +0100

    Add users router

--- routes/users.js
+++ routes/users.js
@@ -3,4 +3,8 @@ const express = require('express');
 const router = express.Router();
 const connexion = require('../data/mysql');
 
+router.get('/users', (req, res) => {
+  res.status(200).send('<h1>You welcome into my app</h1>');
+});
+
 module.exports = router;
